13ad94289055189e5e7c7ea03a142c4e084b7cd5
bphu21 <101784173+bphu21@users.noreply.github.com>
2022-03-17 00:21:25 -0400

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+main_vpc_cidr = "10.1.0.0/16"
+ public_subnets = "10.1.0.0/24"
+ public_subnets_2 = "10.1.1.0/24"
+ private_subnets = "10.1.2.0/24"
+ private_subnets_2 = "10.1.3.0/24"
---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "region" {}
+variable "main_vpc_cidr" {}
+variable "public_subnets" {}
+variable "public_subnets_2" {}
+variable "private_subnets" {}
+variable "private_subnets_2" {}
